57475d6f9f1ddf95cb585f3dccca05a2a7ff8788
Ran <raneeldan@gmail.com>
2020-03-26 21:04:01 +0200

    added class transforming row into vector of features

--- CsvFormater.py
+++ CsvFormater.py
@@ -1,13 +1,16 @@
 import csv
+from rowToVector import RowToVector
 
 filename = "SMSSpamCollection"
 
 with open(filename,'r',encoding="utf8") as csvin,\
         open('tags.csv', 'w', newline='', encoding='utf-8') as csvoutForTags,\
-            open('data.csv', 'w', newline='', encoding='utf-8') as csvoutForData:
+            open('data.csv', 'w', newline='', encoding='utf-8') as csvoutForData,\
+                 open('features.csv', 'w', newline='', encoding='utf-8') as csvoutForFeatures:
     csvin = csv.reader(csvin, delimiter='\t')
     csvoutForTags = csv.writer(csvoutForTags)
     csvoutForData = csv.writer(csvoutForData)
+    csvoutForFeatures = csv.writer(csvoutForFeatures)
     for row in csvin:
         csvoutForTags.writerow([row[0]])
-        csvoutForData.writerow(row[1:])
+        csvoutForFeatures.writerow(RowToVector(row[1:]))
--- rowToVector.py
+++ rowToVector.py
@@ -1,18 +1,41 @@
+from typing import List
+import re
+
 
 class RowToVector:
-    def __init__(self, row : str[]):
-        row = row
-        funcs = []
-        vector =  []
-        numOfFeaturs = 4
-        initFuncs()
+    def __init__(self, row: List[str]):
+        self.row: List[str] = [word for line in row for word in line.split()]
+        self.funcs: List = []
+        self.vector = []
+        self.__init_funcs()
+        for func in self.funcs:
+            self.vector.append(func())
 
+    def __init_funcs(self):
+        self.funcs.append(self.number_of_words)
+        self.funcs.append(self.percentage_of_non_letters)
+        self.funcs.append(self.has_link)
+        self.funcs.append(self.percentage_of_capital_letters)
 
-    def initFuncs(self):
-        for i in range numberOfFeaturs:
-            
+    def number_of_words(self) -> int:
+        return len(self.row)
 
+    def percentage_of_non_letters(self) -> float:
+        non_letter_sign = 0
+        for word in self.row:
+            for index, letter in enumerate(word):
+                if ord(word[index]) > 122 or ord(word[index]) < 65:
+                    non_letter_sign += 1
+        return non_letter_sign / self.number_of_words()
 
-    def numberOfWords(self):
-         return (len(self.row))
-        #self.funcs.append(self.numberOfWords)
+    def has_link(self) -> bool:
+        return re.search(
+            "((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", ''.join(self.row)) is None
+
+    def percentage_of_capital_letters(self) -> float:
+        non_letter_sign = 0
+        for word in self.row:
+            for index, letter in enumerate(word):
+                if ord(word[index]) > 64 or ord(word[index]) < 91:
+                    non_letter_sign += 1
+        return non_letter_sign / self.number_of_words()
