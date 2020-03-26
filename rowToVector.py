57ea53ec2e5ede7aa578ece257736884929227ae
Ran <raneeldan@gmail.com>
2020-03-26 19:42:49 +0200

    save

---
+++ rowToVector.py
@@ -0,0 +1,18 @@
+
+class RowToVector:
+    def __init__(self, row : str[]):
+        row = row
+        funcs = []
+        vector =  []
+        numOfFeaturs = 4
+        initFuncs()
+
+
+    def initFuncs(self):
+        for i in range numberOfFeaturs:
+            
+
+
+    def numberOfWords(self):
+         return (len(self.row))
+        #self.funcs.append(self.numberOfWords)
